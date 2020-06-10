38fec14d6e7264391ad2cc2d3b28eb1f88cb3de7
divya <divyakorat1985@gmail.com>
2020-06-10 23:07:09 +0100

    javaforsthomework

---
+++ src/MyDetail.java
@@ -0,0 +1,21 @@
+import javax.swing.text.AttributeSet;
+
+public class MyDetail
+{
+    public static void main (String[]args)
+    {
+        String name ="Divya Korat";
+        String adress = "204 oaks lane";
+        String mobile = "07877469140";
+        String age = "35";
+        String salary = "35000";
+        String email = "divyakorat@yahoo.co.uk";
+
+        System.out.println(name);
+        System.out.println(adress);
+        System.out.println(mobile);
+        System.out.println(age);
+        System.out.println(salary);
+        System.out.println(email);
+    }
+}
